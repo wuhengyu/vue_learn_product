46c64ab906c004f19dd9c0e5dd4a25ca9fbe6468
吴恒宇 <578060214@qq.com>
2023-07-22 22:44:47 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
+  <!-- 用来放置各路由对应的组件（页面） -->
   <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 };
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 html,
 body,
 ul,
-li,
 h1,
 h2,
 h3,
@@ -20,23 +27,15 @@ p {
   margin: 0;
   padding: 0;
 }
-.w100p{
+.w100p {
   width: 100%;
 }
-.txt-r{
+.txt-r {
   text-align: right;
 }
-.flex-center{
+.flex-center {
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 </style>
--- src/views/sign/login.vue
+++ src/views/sign/login.vue
@@ -19,21 +19,32 @@ export default {
 
 <template>
   <div class="page flex-center">
-    <div class="sian-box">
+    <div class="sign-box">
       <el-form ref="form" :model="form" :rules="rules" label-width="60px">
-          <h3 class="title">系统登录</h3>
+        <h3 class="title">系统登录</h3>
 
-          <el-form-item label="账号" prop="account">
-            <el-input v-model="form.account" placeholder="请输入用户名" prefix-icon="user"></el-input>
-          </el-form-item>
+        <el-form-item label="账号" prop="account">
+          <el-input
+            v-model="form.account"
+            placeholder="请输入用户名"
+            prefix-icon="user"
+          ></el-input>
+        </el-form-item>
 
-          <el-form-item label="密码" prop="password">
-            <el-input v-model="form.password" type="password" placeholder="请输入密码" prefix-icon="lock"></el-input>
-          </el-form-item>
-          
-          <el-form-item label="">
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="form.password"
+            type="password"
+            placeholder="请输入密码"
+            prefix-icon="lock"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item label="">
           <div>
-          <el-button @click="login" type="primary" class="w100p">登录</el-button>
+            <el-button @click="login" type="primary" class="w100p"
+              >登录</el-button
+            >
           </div>
           <div class="txt-r">
             <router-link to="/register">没有账号?去注册</router-link>
@@ -45,37 +56,63 @@ export default {
 </template>
 
 <script>
-export default{
+export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'login',
+  name: "login",
   data() {
     return {
       form: {
-        account: '',
-        password: '',
+        account: "",
+        password: "",
       },
       rules: {
         account: [
-          { required: true, message: '请输入用户名', trigger:  ["blur", "change"]},
-          {pattern: /^[a-zA-Z0-9_-]{3,20}$/, message: '用户名必须是3-20个字母、数字、下划线或减号', trigger: 'blur'},
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: ["blur", "change"],
+          },
+          {
+            pattern: /^[a-zA-Z0-9_-]{3,20}$/,
+            message: "用户名必须是3-20个字母、数字、下划线或减号",
+            trigger: "blur",
+          },
         ],
         password: [
-          { required: true, message: '请输入密码', trigger:  ["blur", "change"]},
-          {pattern: /^[a-zA-Z0-9_-]{3,20}$/, message: '密码必须是3-20个字母、数字、下划线或减号', trigger: 'blur'},
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: ["blur", "change"],
+          },
+          {
+            pattern: /^[a-zA-Z0-9_-]{3,20}$/,
+            message: "密码必须是3-20个字母、数字、下划线或减号",
+            trigger: "blur",
+          },
         ],
       },
     };
   },
   methods: {
     login() {
-      this.$refs.form.validate((valid) => {
-        if (valid) {
-          this.$router.push('/home');
-        } else {
-          return false;
-        }
-      });
+      alert("login");
+      // this.$refs.form.validate((valid) => {
+      //   if (valid){
+      //     const post = ((params) => {
+      //       return new Promise((resolve, reject) => {
+      //         this.$axios.post("/login", params).then((res) => {
+      //           resolve(res);
+      //         });
+      //       });
+      //     })(this.form);
+      //     // })
+      //   }
+      // });
     },
   },
 };
 </script>
+
+<style lang="css">
+@import url(./index.css);
+</style>
