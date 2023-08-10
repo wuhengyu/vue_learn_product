a587fc2625391678d6d1d27f9908507e84df42a0
wuhengyu <578060214@qq.com>
2023-08-10 11:40:40 +0800

    add table data

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,62 @@
 <template>
   <div class="flex">
-    <el-form :model="searchchForm" inline>
+    <el-form :model="searchForm" inline>
       <el-form-item label="商品名称">
-        <el-input v-model="searchForm.name" placeholder="输入商品名称进行查询">
-        </el-input>
+        <el-input
+          v-model="searchForm.name"
+          placeholder="输入商品名称进行查询"
+        ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="handleSearch"></el-button>
+        <el-button type="primary" @click="handleSearch">查询</el-button>
       </el-form-item>
     </el-form>
-    <el-button>新增商品</el-button>
+    <el-button type="primary" @click="handleAdd">新增商品</el-button>
   </div>
 
   <el-table :data="tableData" border style="width: 100%" empty-text="暂无数据">
-    <el-table-column type="index" label="序号" width="60">
-      <el-table-column prop="name" label="商品名称">
-        <el-table-column prop="price" label="单价">
-          <el-table-column prop="count" label="库存">
-            <el-table-column
-              prop="onsale"
-              label="是否上架"
-              :formatter="formatOnSale"
-            >
-              <el-table-column prop="remark" label="备注">
-                <el-table-column
-                  label="添加时间"
-                  :formatter="formatTime"
-                  prop="createTime"
-                >
-                  <el-table-column label="操作">
-                    <template #default="{ row }">
-                      <el-button>
-                        <el-button
-                          type="text"
-                          size="small"
-                          @click="handleEdit(row)"
-                          >编辑</el-button
-                        >
-                        <el-button
-                          type="text"
-                          size="small"
-                          @click="handleDelete(row)"
-                          >删除</el-button
-                        >
-                      </el-button>
-                    </template>
-                  </el-table-column>
-                </el-table-column>
-              </el-table-column>
-            </el-table-column>
-          </el-table-column>
-        </el-table-column>
-      </el-table-column>
+    <el-table-column type="index" label="序号" width="60"></el-table-column>
+    <el-table-column prop="name" label="商品名称"></el-table-column>
+    <el-table-column prop="price" label="单价"></el-table-column>
+    <el-table-column prop="count" label="库存"></el-table-column>
+    <el-table-column
+      prop="onsale"
+      label="是否上架"
+      :formatter="formatterOnSale"
+    ></el-table-column>
+    <el-table-column prop="remark" label="备注"></el-table-column>
+    <el-table-column
+      prop="createTime"
+      label="添加时间"
+      :formatter="formatTime"
+    ></el-table-column>
+    <el-table-column label="操作">
+      <template #default="{ row }">
+        <el-button type="text" size="small" @click="handleEdit(row)"
+          >编辑</el-button
+        >
+        <el-button type="text" size="small" @click="handleDelete(row)"
+          >删除</el-button
+        >
+      </template>
     </el-table-column>
   </el-table>
 
   <el-dialog
     v-model="createVisible"
     :title="title"
-    width="500px"
+    width="500"
     :before-close="handleClose"
   >
     <el-form ref="formRef" :model="createForm" label-width="80px">
-      <!-- {{ createForm }} -->
-      <!-- <el-form-item prop="id" v-show="false">
-      <input v-model="createForm.id" />
-      </el-form-item> -->
       <el-form-item label="商品名称" prop="name">
-        <el-input v-model="createForm.name" placeholder="输入数量"></el-input>
+        <el-input
+          v-model="createForm.name"
+          placeholder="输入商品名称"
+        ></el-input>
       </el-form-item>
       <el-form-item label="单价" prop="price">
-        <el-input v-model="createForm.price" placeholder="输入数量"></el-input>
+        <el-input v-model="createForm.price" placeholder="输入单价"></el-input>
       </el-form-item>
       <el-form-item label="数量" prop="count">
         <el-input v-model="createForm.count" placeholder="输入数量"></el-input>
@@ -89,10 +75,8 @@
 
     <template #footer>
       <span class="dialog-footer">
-        <el-button>
-          <el-button @click="createClose = false">取 消</el-button>
-          <el-button type="primary" @click="handleConfirm">确 定</el-button>
-        </el-button>
+        <el-button @click="handleClose">取消</el-button>
+        <el-button type="primary" @click="handleConfirm">确定</el-button>
       </span>
     </template>
   </el-dialog>
@@ -100,10 +84,12 @@
 
 <script>
 import { reactive, ref, getCurrentInstance } from "vue";
+
 export default {
   name: "App",
   setup() {
     const { proxy } = getCurrentInstance();
+
     const formatTime = (row, col) => {
       const date = new Date(row[col.property]);
       const year = date.getFullYear();
@@ -112,20 +98,114 @@ export default {
       const hour = date.getHours();
       const min = date.getMinutes();
       const sec = date.getSeconds();
-      return "${year}-${month}-${day} ${hour}:${min}:${sec}";
+      return `${year}-${month}-${day} ${hour}:${min}:${sec}`;
     };
+
     const formatterOnSale = (row, col) => {
       return row[col.property] ? "是" : "否";
     };
 
-    // 后台数据模拟
+    // New Product
+    const formRef = ref(null);
+    const title = ref(null);
+    const createVisible = ref(false);
+    const createForm = reactive({
+      id: "",
+      name: "",
+      price: "",
+      count: "",
+      onsale: false,
+      remark: "",
+    });
+    const defaultForm = { ...createForm };
+
+    const onCreate = (info) => {
+      setTimeout(() => {
+        // Simulating API request
+        const res = {
+          code: 200,
+          msg: "",
+        };
+        if (res.code !== 200) {
+          proxy.$message.error(res.msg);
+        } else {
+          const newInfo = {
+            ...info,
+            createTime: new Date(),
+            id: `id_${Date.now()}`,
+          };
+          data.unshift(newInfo);
+          proxy.$message.success("新增成功");
+          handleClose();
+          getTableData();
+        }
+      }, 300);
+    };
+
+    const handleClose = () => {
+      createVisible.value = false;
+      for (const key in createForm) {
+        createForm[key] = defaultForm[key];
+      }
+    };
+
+    const handleConfirm = () => {
+      if (createForm.id) {
+        onUpdate(createForm);
+      } else {
+        onCreate(createForm);
+      }
+    };
+
+    const handleAdd = () => {
+      title.value = "新增商品";
+      createVisible.value = true;
+    };
+
+    // Update Product
+    const onUpdate = (newInfo) => {
+      setTimeout(() => {
+        // Simulating API request
+        const res = {
+          code: 200,
+          msg: "",
+        };
+        if (res.code !== 200) {
+          proxy.$message.error(res.msg);
+        } else {
+          const index = data.findIndex((v) => v.id === newInfo.id);
+          data[index] = {
+            ...data[index],
+            ...newInfo,
+          };
+          proxy.$message.success("修改成功");
+          handleClose();
+          getTableData();
+        }
+      }, 300);
+    };
+
+    const handleEdit = (row) => {
+      title.value = "修改商品";
+      createVisible.value = true;
+      for (const key in createForm) {
+        createForm[key] = row[key];
+      }
+    };
+
+    const handleDelete = (row) => {
+      const index = data.findIndex((v) => v.id === row.id);
+      data.splice(index, 1);
+      proxy.$message.success("删除成功");
+      getTableData();
+    };
+
+    // Mock backend data
     const data = [];
     const tableData = ref([]);
     const getTableData = () => {
       setTimeout(() => {
-        // 失败
-        // const res = {code :-1, msg: '系统异常'}
-        // 成功
+        // Simulating API request
         const res = {
           code: 200,
           msg: "",
@@ -141,28 +221,32 @@ export default {
         tableData.value = res.data;
       }, 300);
     };
+
     const searchForm = reactive({
       name: "",
     });
+
     const handleSearch = () => {
       getTableData();
     };
+
     getTableData();
+
     return {
       handleSearch,
       formatTime,
       formatterOnSale,
       tableData,
       searchForm,
-      formRef: ref(null),
-      title: ref(null),
-      createForm: ref(null),
-      createVisible: ref(null),
-      handleAdd: ref(null),
-      handleClose: ref(null),
-      handleDelete: ref(null),
-      handleEdit: ref(null),
-      handleConfirm: ref(null),
+      formRef,
+      title,
+      createForm,
+      createVisible,
+      handleAdd,
+      handleClose,
+      handleDelete,
+      handleEdit,
+      handleConfirm,
     };
   },
 };
@@ -173,28 +257,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #2c3e50; */
-}
-html,
-body,
-ul,
-h1,
-h2,
-h3,
-p {
-  margin: 0;
-  padding: 0;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
-.flex-center {
+.flex {
   display: flex;
   align-items: center;
-  justify-content: center;
-}
-
-.register-link .el-form-item__content {
-  display: flex;
-  justify-content: right;
+  justify-content: space-between;
 }
 </style>
